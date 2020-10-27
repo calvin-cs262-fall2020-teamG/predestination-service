0717e10b576b70b9a4f145f6a8f4a4772f80e151
ewalters47 <emw45@students.calvin.edu>
2020-10-27 17:19:18 -0400

    update .gitignore for node_modules

---
+++ predestinationService.js
@@ -0,0 +1,35 @@
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: "raja.db.elephantsql.com",
+    port: 5432,
+    database: process.env.USER,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+});
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+router.use(express.json());
+
+router.get("/", readHelloMessage);
+
+function errorHandler(err, req, res) {
+    if (app.get('env') === "development") {
+        console.log(err);
+    }
+    res.sendStatus(err.status || 500);
+}
+
+function returnDataOr404(res, data) {
+    if (data == null) {
+        res.sendStatus(404);
+    } else {
+        res.send(data);
+    }
+}
+
+function readHelloMessage(req, res) {
+    res.send('Hello, CS 262 Monopoly service!');
+}
