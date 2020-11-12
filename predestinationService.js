b9559ec8143da81742c6a45c9590c3060bdcb709
ewalters47 <emw45@students.calvin.edu>
2020-11-11 23:30:39 -0500

    fix heroku deployment

---
+++ predestinationService.js
@@ -0,0 +1,37 @@
+/**
+ * REST-inspired webservice for the Predestination DB
+ * @author: Ethan Walters
+ * @date: 10/27/2020
+ */
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: "raja.db.elephantsql.com",
+    port: 5432,
+    database: process.env.USER,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+});
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+router.use(express.json());
+
+router.get("/", readHelloMessage);
+
+app.use(router);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+function errorHandler(err, req, res) {
+    if (app.get('env') === "development") {
+        console.log(err);
+    }
+    res.sendStatus(err.status || 500);
+}
+
+function readHelloMessage(req, res) {
+    res.send('Hello, Predestination Service!');
+}
