410edc37c7981dd583249a9ed4497dc1cedbe19e
Jacob Brink <jacobpeterbrink@gmail.com>
2020-11-19 21:45:41 -0500

    Added basic routes necessary for a simple game

--- predestinationService.js
+++ predestinationService.js
@@ -26,7 +26,13 @@ router.use(express.json());
 /** Setup express routes */
 router.get("/", readHelloMessage);
 router.get("/clues", readClues);
-router.get("/clues/:id", readClue);
+router.get("/clues/:clueid", readClue);
+router.get("/user/:googleid/signin/", signInUser);
+router.get("/user/:googleid/profile/", getUserData);
+router.get("/game/:gameid/players", getGamePlayers);
+router.get("/game/:gameid/seeker/:googleid/clues", getPlayerClues);
+router.get("/game/:gameid/seeker/:googleid/addpoints/:clueid/:time", updatePlayerClues);
+
 
 app.use(router);
 app.use(errorHandler);
@@ -37,6 +43,60 @@ function errorHandler(err, req, res) {
         console.log(err);
     }
     res.sendStatus(err.status || 500);
+}
+
+// @params: Google userid
+// Postcondition: if user does not exist in table, add user
+const signInUser = async (req, res, next) => {
+    try {
+        await db.none('INSERT INTO Player(ID, name, profilePictureURL) VALUES (${googleid}, ${name}, ${profilePictureURL}) ON DUPLICATE KEY UPDATE SET profilePictureURL=${profilePictureURL}, name=${name}', req.body);
+    } catch (err) {
+        next(err);
+    }
+}
+
+// @params: user ID
+// returns: user name, user photo URL
+const getUserData = async  (req, res, next) => {
+    try {
+        const data = await db.oneOrNone(
+            `SELECT * FROM Player
+            WHERE ID=${googleid}`
+        );
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }    
+}
+
+// @params: user ID,
+// returns: list of clues, noting which ones were received and not received
+const getPlayerClues = async (req, res, next) => {
+    try {
+        const data = await db.many("SELECT * FROM CluePlayer, Clue WHERE CluePlayer.playerID=${googleid} AND WHERE CluePlayer.ClueID=Clue.ID AND WHERE Clue.gameID=${gameid}", req.body);
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }
+}
+
+// called when player unlocked a clue
+const updatePlayerClues = async (req, res, next) => {
+    try {
+        await db.none("INSERT IGNORE INTO CluePlayer (ClueID, playerID, time) VALUES (${clueid}, ${googleid}, ${time})", req.body);
+    } catch (err) {
+        next(err);
+    }
+}
+
+// get list of players with game code
+const getGamePlayers = async (req, res, next) => {
+    try {
+        const data = await db.many("SELECT playerID FROM PlayerGame WHERE gameID=${gameid}", req.body);
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }
 }
 
 /** If data isn't found return 404 otherwise, send required data */
@@ -66,7 +126,7 @@ function readClues(req, res, next) {
 
 /** Reads and returns a clue by its ID */
 function readClue(req, res, next) {
-    db.oneOrNone('SELECT * FROM Clue WHERE id=${id}', req.params)
+    db.oneOrNone('SELECT * FROM Clue WHERE id=${clueid}', req.params)
     .then(data => {
         returnDataOr404(res, data);
     })
