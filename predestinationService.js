4f1d72b7dd4cd00479ed2915fe2b848ffabee15c
ewalters47 <emw45@students.calvin.edu>
2020-12-05 16:22:33 -0500

    Move server code to separate file and restructure code format

--- predestinationService.js
+++ predestinationService.js
@@ -6,68 +6,30 @@
 
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: "raja.db.elephantsql.com",
-    port: 5432,
+    host: process.env.DB_SERVER,
+    port: process.env.PORT,
     database: process.env.USER,
     user: process.env.USER,
     password: process.env.PASSWORD
 });
 
 /** Setup express server */
-
 const express = require('express');
 const app = express();
+const router = express.Router();
+router.use(express.json());
 
+app.use(router);
+app.use(errorHandler);
 
 /** Check port in environment variable first, otherwise run on 3000 */
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-/* Game Server Code
- * @author: Jacob Brink
- * 
- * Completed: 
- *  - clients can join game and get current game state
- *  - clients can update others
- * 
- * Todo:
- *  - finish joinGame database selection to retreive player data along with a sum of their points
- *  - handle player quitting
- *  - test with and connect to client side
- */
-
-
-const server = require('http').createServer(app);
-const options = {};
-const io = require('socket.io')(server, options);
-
-io.on('connection', socket => {
-    
-    socket.on('join-session', (gameCode, playerID) => {
-        joinGame(socket, gameCode, playerID);
-    });
-    
-    // updates all players in a game sesion with new information
-    socket.on('found-clue', (gameCode, playerID, clueID, timeStamp) => {
-        socket.to(gameCode).emit('update', playerID, clueID, timeStamp);
-    });
-
-});
-
-/* joinGame prepares player for game by giving current snapshot of game to client and subscribing them to the room identified by the gameCode
- */
-const joinGame = async (socket, gameCode, playerID) => {
-    const data = await db.many(`SELECT PlayerID, name, profilePictureURL, points FROM PlayerGame, Player, CluePlayer, Clue WHERE playerID=Player.ID AND gameID=${gameCode} AND Clue.gameID=${gameCode}`); //todo
-    socket.emit('players-snapshot', data);
-    socket.join(gameCode);
+// Sends a simple hello message - for testing
+function readHelloMessage(req, res) {
+    res.send('Hello, Predestination Service!');
 }
-
-const router = express.Router();
-router.use(express.json());
-
-/** Setup express routes */
-
-
 
 function errorHandler(err, req, res) {
     if (app.get('env') === "development") {
@@ -76,18 +38,36 @@ function errorHandler(err, req, res) {
     res.sendStatus(err.status || 500);
 }
 
-// @params: Google userid
-// Postcondition: if user does not exist in table, add user
+/*
+returnDataOr404 sends required data unless its state is null
+@params: data-
+ */
+function returnDataOr404(res, data) {
+    if (data == null) {
+        res.sendStatus(404);
+    } else {
+        res.send(data);
+    }
+}
+
+/*
+signInUser updates the Player table with corresponding Google account data
+Postcondition: if user does not exist in table, add user
+@params: Google userid
+ */
 const signInUser = async (req, res, next) => {
     try {
         await db.none('INSERT INTO Player(ID, name, profilePictureURL) VALUES (${googleid}, ${name}, ${profilePictureURL}) ON DUPLICATE KEY UPDATE SET profilePictureURL=${profilePictureURL}, name=${name}', req.body);
     } catch (err) {
         next(err);
     }
-}
+};
 
-// @params: user ID
-// returns: user name, user photo URL
+/*
+getUserData retrieves Google account data from the Player data
+@params: user ID
+returns: username, photo URL
+ */
 const getUserData = async  (req, res, next) => {
     try {
         const data = await db.oneOrNone(
@@ -97,7 +77,7 @@ const getUserData = async  (req, res, next) => {
         res.send(data);
     } catch (err) {
         next(err);
-    }    
+    }
 }
 
 // @params: user ID,
@@ -130,41 +110,27 @@ const getGamePlayers = async (req, res, next) => {
     }
 }
 
-/** If data isn't found return 404 otherwise, send required data */
-function returnDataOr404(res, data) {
-    if (data == null) {
-        res.sendStatus(404);
-    } else {
-        res.send(data);
-    }
-}
-
-/** Sends a simple hello message - for testing */
-function readHelloMessage(req, res) {
-    res.send('Hello, Predestination Service!');
-}
-
-/** Reads and returns all clues in the Clue table */
+// Reads and returns all clues in the Clue table
 function readClues(req, res, next) {
     db.many("SELECT * FROM Clue")
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        next(err);
-    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            next(err);
+        })
 }
 
-/** Reads and returns a clue by its ID */
+// Reads and returns a clue by its ID
 function readClue(req, res, next) {
     db.oneOrNone('SELECT * FROM Clue WHERE id=${clueid}', req.params)
-    .then(data => {
-        returnDataOr404(res, data);
-    })
-    .catch(err => {
-        next(err);
-    })
-};
+        .then(data => {
+            returnDataOr404(res, data);
+        })
+        .catch(err => {
+            next(err);
+        });
+}
 
 router.get("/", readHelloMessage);
 router.get("/clues", readClues);
@@ -174,9 +140,3 @@ router.get("/user/:googleid/profile/", getUserData);
 router.get("/game/:gameid/players", getGamePlayers);
 router.get("/game/:gameid/seeker/:googleid/clues", getPlayerClues);
 router.get("/game/:gameid/seeker/:googleid/addpoints/:clueid/:time", updatePlayerClues);
-
-
-
-
-app.use(router);
-app.use(errorHandler);
