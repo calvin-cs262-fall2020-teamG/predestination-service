23cee7debf086921f920da9564a1866d64daa1b4
ewalters47 <emw45@students.calvin.edu>
2020-12-05 20:13:49 -0500

    Change database connection variables

--- predestinationDirect.js
+++ predestinationDirect.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: "raja.db.elephantsql.com",
-    port: 5432,
-    database: process.env.USER,
-    user: process.env.USER,
-    password: process.env.PASSWORD
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 db.many("SELECT * FROM Clue")
