6b21910e4dc5c676b6b98694a5a477cfa12005ae
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2020-12-12 07:38:26 +0000

    Reduce database concurrency to 2.
    Hopefully fixes too many connections error.
    
    The default concurrency of pg-promise is 10 which is greater than the
    5 allowed by ElephantSQL.

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ const db = pgp({
     port: process.env.DB_PORT,
     database: process.env.DB_USER,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    // The “tiny turtle” plan https://www.elephantsql.com/plans.html only allows up to 5 concurrent connections.
+    // Ensure there are way fewer than that in case we run multiple instances.
+    max: 2,
 });
 module.exports = db;
