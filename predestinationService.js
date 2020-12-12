8ff91cfe3bedb927c80074d5797783ee3925460d
Jacob-Brink <jacobpeterbrink@gmail.com>
2020-12-12 10:14:39 -0500

    Revert "cleanup"
    This reverts commit f4b98dd40a8f571536b534023de8fd19a98a286f.

--- predestinationService.js
+++ predestinationService.js
@@ -119,13 +119,10 @@ async function deliverSnapshot(socket, gameCode, t) {
 async function addClue(gameCode, playerID, clueID, timeStamp, socket, t) {
     console.log(`Player ${playerID} found clue ${clueID}! Congratulations!`);
     try {
-        console.log('Insert clue and player into CluePlayer database');
-        console.log(`INSERT INTO CluePlayer (ClueID, playerID, time) VALUES (${clueID}, ${playerID}, ${timeStamp})`);
-        await t.none(`INSERT INTO CluePlayer (ClueID, playerID, time) VALUES (${clueID}, ${playerID}, ${timeStamp})`);
-        console.log('Marker 1');
-	    io.to(gameCode).emit('update', playerID, clueID, timeStamp);
+	await t.none(`INSERT INTO CluePlayer (ClueID, playerID, time) VALUES (${clueID}, ${playerID}, ${timeStamp})`);
+	io.to(gameCode).emit('update', playerID, clueID, timeStamp);
     } catch (e) {
-	    console.log(e);
+	console.log(e);
     }
     
 }
